0d09134f11bd3d9c19946e7afd5a2c7190913e22
karthik-k-18 <200010024@iitdh.ac.in>
2022-11-30 23:38:08 +0530

    removed secret key used in production

--- MySite/settings.py
+++ MySite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*kh63n3u+467f^xlsrfzq##*d=39*ns&13n_==njx0k5&u4vfz"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-#import http response
+# import http response
 from django.http import HttpResponse
 from datetime import date
 
@@ -70,33 +70,36 @@ posts = [
         """
     },
     {
-        "slug":"stephen-curry",
-        "image":"curry.jpg",
-        "author":"Stephen Curry",
-        "date":date(2021, 7, 21),
-        "title":"Favorite Basketball Player",
-        "excerpt":"I love basketball and my favorite player is Stephen Curry",
-        "content":"""
+        "slug": "stephen-curry",
+        "image": "curry.jpg",
+        "author": "Stephen Curry",
+        "date": date(2022, 11, 21),
+        "title": "Favorite Basketball Player",
+        "excerpt": "I love basketball and my favorite player is Stephen Curry",
+        "content": """
         Wardell Stephen Curry II (born March 14, 1988) is an American professional basketball player who plays for the Golden State Warriors of the National Basketball Association (NBA). He is a three-time NBA champion, two-time NBA Most Valuable Player, and three-time NBA All-Star. He is widely regarded as one of the greatest shooters in NBA history. He is the son of former NBA player Dell Curry and the younger brother of former NBA player Seth Curry. He is married to television personality Ayesha Curry.
         """
     }
 ]
 
+
 def few_posts(request):
-    #get latest 3 posts in a list sorted on date
+    # get latest 3 posts in a list sorted on date
     latest_posts = sorted(posts, key=lambda k: k['date'], reverse=True)[:3]
-    #pass the list to the template
+    # pass the list to the template
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
+
+
 def all_posts(request):
-    return render(request, "blog/all-posts.html",{
+    return render(request, "blog/all-posts.html", {
         "posts": posts
     })
+
 
 def post_detail(request, slug):
     identified_post = next(post for post in posts if post["slug"] == slug)
     return render(request, "blog/post-detail.html", {
         "post": identified_post
     })
-
